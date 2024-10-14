6885b6649cdd1c0d584fdc58de4230979475a3d3
이강호 <91510831+gleehave@users.noreply.github.com>
2024-10-14 22:37:05 +0900

    Title: 행복한 수열의 개수, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241014/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,71 @@
+import java.io.*;
+
+public class Main {
+
+    static int count;
+    static int N, M;
+    static int[][] map;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] data = br.readLine().split(" ");
+        N = Integer.parseInt(data[0]);
+        M = Integer.parseInt(data[1]);
+
+        map = new int[N][N];
+
+        for(int i=0; i<N; i++){
+            String[] mapData = br.readLine().split(" ");
+            for(int j=0; j<N; j++){
+                map[i][j] = Integer.parseInt(mapData[j]);
+            }
+        }
+
+        count = 0;
+        for(int r=0; r < N; r++){
+            for(int c=0; c <= N-M; c++){
+                
+                boolean used = false;
+                int check = 1;
+                for(int k=c; k<c+M-1; k++){
+                    if (map[r][k] == map[r][k+1]){
+                        check++;
+                    } else {
+                        check = 1;
+                        continue;
+                    }
+                    if (check >= M){
+                        used = true;
+                        count++;
+                    }
+                }
+
+                if (used) break;
+            }
+        }
+
+        for(int c=0; c < N; c++){
+            for(int r=0; r <= N-M; r++){
+
+                boolean used = false;
+                int check = 1;
+                for(int k=r; k<r+M-1; k++){
+                    if (map[k][c] == map[k+1][c]) {
+                        check++;
+                    } else {
+                        check = 1;
+                        continue;
+                    }
+                    if (check >= M){
+                        count++;
+                        used = true;
+                        break;
+                    } 
+                }
+                if (used) break;
+            }
+        }
+        System.out.println(count);
+    
+    }
+}
