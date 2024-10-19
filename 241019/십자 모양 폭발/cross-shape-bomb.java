6ca740c0e7f6c42405c90e71b18f60359efcc9e2
이강호 <91510831+gleehave@users.noreply.github.com>
2024-10-19 12:21:59 +0900

    Title: 십자 모양 폭발, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241019/십자 모양 폭발/cross-shape-bomb.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] grid;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1}; // 상하좌우
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        grid = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken())-1;
+        int c = Integer.parseInt(st.nextToken())-1;
+
+        for(int d=0; d<4; d++){
+            for(int move=0; move<grid[r][c]-1; move++){
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+                grid[nr][nc] = 0;
+            }
+        }
+        grid[r][c] = 0;
+
+        for(int j=0; j<n; j++){
+            Stack<Integer> stack = new Stack<>();
+            for(int i=0; i<n; i++){
+                if (grid[i][j] == 0) continue;
+                stack.push(grid[i][j]);
+                grid[i][j] = 0;
+            }
+
+            for(int i=n-1; i>=0; i--){
+                if (!stack.isEmpty()){
+                    grid[i][j] = stack.pop();
+                }
+            }
+         }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(grid[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
