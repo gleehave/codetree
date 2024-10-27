f976e0b7958c7676ab65fbb81c18742a57193641
이강호 <91510831+gleehave@users.noreply.github.com>
2024-10-28 08:04:45 +0900

    Title: 그래프 탐색, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241028/그래프 탐색/graph-traversal.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m, count;
+    static List<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n];
+        graph = new ArrayList[n];
+        for(int i=0; i<n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken())-1;
+            int to = Integer.parseInt(st.nextToken())-1;
+            graph[from].add(to);
+            graph[to].add(from);
+        } // graph 완료
+
+        count = -1;
+        solve(0); // 1번 정점 부터 시작
+        System.out.println(count);
+    }
+
+    public static void solve(int vertex){
+        
+        for(int i=0; i<graph[vertex].size(); i++){
+            int now = graph[vertex].get(i);
+            if (!visited[now]){
+                count++;
+                visited[now] = true;
+                solve(now);
+            } 
+        }
+    }
+}
