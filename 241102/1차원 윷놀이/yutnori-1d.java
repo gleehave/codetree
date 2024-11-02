c2df5aa13029fd0e19827e24d8f9c247dc264588
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-02 14:50:30 +0900

    Title: 1차원 윷놀이, Time: 1226ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241102/1차원 윷놀이/yutnori-1d.java
+++ 241102/1차원 윷놀이/yutnori-1d.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class Main {
 
     static int n, m, k;
-    static result;
+    static int result;
     static int[] move;
 
-    static Map<Integer, Integer> score = new HashMap<>();
+    static Map<Integer, Integer> score;
     static List<Integer> select = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
@@ -25,26 +25,28 @@ public class Main {
             move[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=1; i<=k; i++){
-            // 현재 말 , 현재 위치 (1 ~ m)
-            score.put(i, score.getOrDefault(i, 0) + 1);
-        }
-
         result = Integer.MIN_VALUE;
         
         solve(0);
+        System.out.println(result);
     }
 
     public static void solve(int depth){
         if (depth == n){ // turn이 끝났다.
-            
+
+            score = new HashMap<>();
+            for(int i=1; i<=k; i++){
+               // 현재 말 , 현재 위치 (1 ~ m)
+                score.put(i, score.getOrDefault(i, 0) + 1);
+            }
+
             int count = 0;
             for(int i=0; i<select.size(); i++){
                 if (score.get(select.get(i)) >= m) continue;
                 if (score.get(select.get(i)) + move[i] >= m) {
                     count++;        
                 }
-                score.put(score.get(select.get(i)), score.get(select.get(i)) + move[i]);
+                score.put(select.get(i), score.get(select.get(i)) + move[i]);
             }
             result = Math.max(result, count);
             return;
