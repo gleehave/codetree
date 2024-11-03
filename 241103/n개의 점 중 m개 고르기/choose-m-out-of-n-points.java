89cc772be97d0ff7462edf328139a01eb6335be1
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-03 22:23:48 +0900

    Title: n개의 점 중 m개 고르기, Time: 134ms, Memory: 9MB, Status: Passed - CodeTree

--- 241103/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 241103/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -16,45 +16,48 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         points = new int[n][2];
-        for(int i=0; i<n; i++){
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             points[i][0] = Integer.parseInt(st.nextToken());
-            points[i][1] = Integer.parseInt(st.nextToken());            
+            points[i][1] = Integer.parseInt(st.nextToken());
         }
 
         min = Integer.MAX_VALUE;
         solve(0, 0);
-        System.out.println(min);
+        System.out.println(min); // 유클리디안 거리로 출력
     }
 
-    public static void solve(int depth, int cur){
-        if (depth == m){
+    public static void solve(int depth, int cur) {
+        if (depth == m) {
             calc();
             return;
         }
 
-        for(int i=cur; i<n ;i++){
+        for (int i = cur; i < n; i++) {
             select.add(i);
-            solve(depth+1, i+1);
-            select.remove(select.size()-1);
+            solve(depth + 1, i + 1);
+            select.remove(select.size() - 1);
         }
     }
 
-    public static void calc(){
+    public static void calc() {
+        int maxDist = 0; // 선택된 점들 중 가장 큰 거리값
 
-        for(int i=0; i<m-1; i++){
-            for(int j=i+1; j<m; j++){
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = i + 1; j < m; j++) {
                 int x1 = points[select.get(i)][0];
-                int y1 = points[select.get(i)][1];         
+                int y1 = points[select.get(i)][1];
                 int x2 = points[select.get(j)][0];
-                int y2 = points[select.get(j)][1];         
+                int y2 = points[select.get(j)][1];
 
-                int x = Math.abs(x1 - x2) * Math.abs(x1 - x2);
-                int y = Math.abs(y1 - y2) * Math.abs(y1 - y2);
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                int dist = dx * dx + dy * dy;
 
-                int dist = x + y;
-                min = Math.min(min, dist);
+                maxDist = Math.max(maxDist, dist);
             }
         }
+
+        min = Math.min(min, maxDist); // 최소의 최대 거리값을 갱신
     }
 }
