b51af3bd5fc7d4f7fdee292853dd893a6df7a3b0
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-03 12:02:28 +0900

    Title: 최소 점프 횟수, Time: 310ms, Memory: 74MB, Status: Runtime Error - CodeTree

---
+++ 241103/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,40 @@
+/*
+2 3 1 1 4 에서 2는 1칸, 2칸 까지 가능한 것.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, min;
+    static int[] jump;
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        jump = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            jump[i] = Integer.parseInt(st.nextToken());
+        }
+
+        min = -1;
+        solve(0, 0);
+        System.out.println(min);
+    }
+
+    public static void solve(int current, int move){
+        if (current == n){
+            min = Math.min(min, move);
+            return;
+        }
+
+        for(int i=current; i<=current+jump[current]; i++){
+            solve(i, move+1);
+        }
+        
+    }
+}
