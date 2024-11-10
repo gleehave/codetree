4fc2a83254d64b3c55664db76be59ba773c810a5
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-10 12:04:14 +0900

    Title: 나이트, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241110/나이트/knight-movements.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int startR, startC, endR, endC;
+    static boolean[][] visited;
+    static int[] dr = {-1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] dc = {-2, -1, 1, 2, -2, -1, 1, 2};
+    static Queue<int[]> queue = new LinkedList<>();
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n][n];
+
+        st = new StringTokenizer(br.readLine());
+        startR = Integer.parseInt(st.nextToken())-1;
+        startC = Integer.parseInt(st.nextToken())-1;
+        endR = Integer.parseInt(st.nextToken())-1;
+        endC = Integer.parseInt(st.nextToken())-1;
+
+        System.out.println(simulate());
+    }
+
+    public static int simulate(){
+        queue.offer(new int[]{startR, startC, 0});
+        visited[startR][startC] = true;
+
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+
+            for(int d=0; d<dr.length; d++){
+                int nextR = cur[0] + dr[d];
+                int nextC = cur[1] + dc[d];
+
+                if (nextR == endR && nextC == endC){
+                    return cur[2]+1;
+                }
+
+                if (nextR < 0 || nextC < 0 || nextR >= n || nextC >= n) continue;
+                if (visited[nextR][nextC]) continue;
+
+                visited[nextR][nextC] = true;
+                queue.offer(new int[]{nextR, nextC, cur[2]+1});
+            }
+        }
+        return -1;
+    }
+}
