458efa022d37158dfd381f92e6dbfdaa0486a0d1
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-10 17:10:44 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 105ms, Memory: 9MB, Status: Passed - CodeTree

--- 241110/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 241110/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -3,11 +3,13 @@ import java.util.*;
 
 public class Main {
     static int n;
+    static boolean[] number;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
+        number = new boolean[1000001];
+        number[0] = true;
         System.out.println(solve());
     }
 
@@ -24,14 +26,19 @@ public class Main {
         while(!queue.isEmpty()){
 
             int[] cur = queue.poll();
+
             if (cur[0] == 1){
                 return cur[1];
             }
 
-            queue.offer(new int[]{cur[0] - 1, cur[1]+1});
-            queue.offer(new int[]{cur[0] + 1, cur[1]+1});
+            if (number[cur[0]]){
+                continue;
+            }
+            number[cur[0]] = true;
+            if (cur[0] - 1 >= 1) queue.offer(new int[]{cur[0] - 1, cur[1]+1});
+            if (cur[0] + 1 <= 1000000) queue.offer(new int[]{cur[0] + 1, cur[1]+1});
             if (cur[0] % 2 == 0) queue.offer(new int[]{cur[0] / 2, cur[1]+1});
-            if (cur[0] % 3 == 0) queue.offer(new int[]{cur[0] / 3, cur[1]+1});   
+            if (cur[0] % 3 == 0) queue.offer(new int[]{cur[0] / 3, cur[1]+1});
         }
         return -1;
     }
