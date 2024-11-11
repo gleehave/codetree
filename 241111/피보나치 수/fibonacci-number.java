483ee9236e1b1bc780b1e90015b4e5f4dc539296
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-11 22:25:55 +0900

    Title: 피보나치 수, Time: 86ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241111/피보나치 수/fibonacci-number.java
@@ -0,0 +1,33 @@
+/*
+N=5
+1 1 2 3 5
+
+
+memo[i] = memo[i-2] + memo[i-1]
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int n;
+    static int[] memo;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        memo = new int[n+1];
+
+        memo[1] = 1;
+        memo[2] = 1;
+
+        for(int i=3; i<=n; i++){
+            memo[i] = memo[i-1] + memo[i-2];
+        }
+
+        System.out.println(memo[n]);
+    }
+}
