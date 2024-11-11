339f735699d7e22fe99f0c47a43b2aced3d15b77
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-11 22:32:51 +0900

    Title: 피보나치 수, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 241111/피보나치 수/fibonacci-number.java
+++ 241111/피보나치 수/fibonacci-number.java
@@ -20,14 +20,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         memo = new int[n+1];
+        if (n==1){
+            System.out.println(1);
+        } else {
+            memo[1] = 1;
+            memo[2] = 1;
 
-        memo[1] = 1;
-        memo[2] = 1;
+            for(int i=3; i<=n; i++){
+                memo[i] = memo[i-1] + memo[i-2];
+            }
 
-        for(int i=3; i<=n; i++){
-            memo[i] = memo[i-1] + memo[i-2];
+            System.out.println(memo[n]);
         }
-
-        System.out.println(memo[n]);
     }
 }
