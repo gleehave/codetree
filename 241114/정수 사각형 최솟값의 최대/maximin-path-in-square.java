6f376de283c91e6390059e27d2d757496ec759dc
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-14 21:08:23 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 101ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241114/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[][] grid;
+    static int[][] memo;
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        
+        memo = new int[n][n];
+        grid = new int[n][n];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        } // grid
+
+        if (n==1){
+            System.out.println(grid[0][0]);
+        } else {
+            memo[0][0] = grid[0][0];
+            for(int c=1; c<n; c++){
+                memo[0][c] = Math.min(grid[0][c-1], grid[0][c]);
+            }
+
+            for(int r=1; r<n; r++){
+                memo[r][0] = Math.min(grid[r-1][0], grid[r][0]);
+            }
+
+            for(int r=1; r<n; r++){
+                for(int c=1; c<n; c++){
+                    memo[r][c] = Math.min(memo[r-1][c], memo[r][c-1]);
+                }
+            }
+
+            System.out.println(Math.max(memo[n-2][n-1], memo[n-1][n-2]));
+        }
+    }
+}
