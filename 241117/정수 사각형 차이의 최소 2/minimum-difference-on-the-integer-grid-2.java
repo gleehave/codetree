89d8f71175059b056f96662b0ecec17e8130498d
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-17 13:20:25 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 98ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241117/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 241117/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -2,9 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, min, max;
+    static int n;
     static int[][] grid;
     static int[][] memo;
+
+    static int[][] min;
+    static int[][] max;
 
     public static void main(String[] args) throws Exception {
         StringTokenizer st;
@@ -12,6 +15,8 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
 
+        min = new int[n][n];
+        max = new int[n][n];
         memo = new int[n][n];
         grid = new int[n][n];
         for(int i=0; i<n; i++){
@@ -21,18 +26,50 @@ public class Main {
             }
         } // grid 입력
 
-        min = Integer.MAX_VALUE;
-        max = Integer.MIN_VALUE;
-        memo[0][0] = grid[0][0];
+        min[0][0] = grid[0][0];
+        max[0][0] = grid[0][0];
 
         for(int c=1; c<n; c++){
-            memo[0][c] = Math.min(memo[0][c-1], grid[0][c]);
+            min[0][c] = Math.min(min[0][c-1], grid[0][c]);
         }
 
         for(int r=1; r<n; r++){
-            memo[r][0] = Math.min(memo[r-1][0], grid[r][0]);
+            min[r][0] = Math.min(min[r-1][0], grid[r][0]);
         }
+
+        for(int c=1; c<n; c++){
+            max[0][c] = Math.max(max[0][c-1], grid[0][c]);
+        }
+
+        for(int r=1; r<n; r++){
+            max[r][0] = Math.max(max[r-1][0], grid[r][0]);
+        }
+
+        for(int r=1; r<n; r++){
+            for(int c=1; c<n; c++){
+                min[r][c] = Math.min(Math.min(min[r-1][c], min[r][c-1]), grid[r][c]);
+                
+            }
+        }
+
+        for(int r=1; r<n; r++){
+            for(int c=1; c<n; c++){
+                max[r][c] = Math.max(Math.max(max[r-1][c], max[r][c-1]), grid[r][c]);
+            }
+        }
+
+        PRINT(min);
+        System.out.println();
+        PRINT(max);
+        
     }
 
-    
+    public static void PRINT(int[][] map){
+        for(int r=0; r<n; r++){
+            for(int c=0; c<n; c++){
+                System.out.print(map[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
