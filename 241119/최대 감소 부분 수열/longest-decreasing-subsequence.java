7fbbe36e157b52e480c8547bf7c33e8b991f498c
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-19 12:06:24 +0900

    Title: 최대 감소 부분 수열, Time: 120ms, Memory: 9MB, Status: Passed - CodeTree

--- 241119/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 241119/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -18,14 +18,12 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = Integer.MIN_VALUE;
+            dp[i] = 1;
         }
 
         dp[0] = 1;
         for(int i=1; i<n; i++){
             for(int j=0; j<i; j++){
-                if (dp[j] == Integer.MIN_VALUE) continue;
-
                 if (arr[j] > arr[i]){
                     dp[i] = Math.max(dp[j] + 1, dp[i]);
                 }
