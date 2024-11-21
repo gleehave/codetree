a3b2037cf5e46bbe3aee8056debdd2865dd011e6
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-21 21:15:42 +0900

    Title: 알바로 부자 되기, Time: 82ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241121/알바로 부자 되기/being-rich-by-working-part-time.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+class Job implements Comparable<Job>{
+    int s, e, p;
+
+    public Job(int s, int e, int p){
+        this.s = s;
+        this.e = e;
+        this.p = p;
+    }
+
+    public int compareTo(Job b){
+        return this.e - b.e;
+    }
+}
+
+
+public class Main {
+    static int n;
+    static Job[] job;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n];
+        job = new Job[n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            job[i] = new Job(
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken())                          
+            );
+            dp[i] = Integer.MIN_VALUE;
+        }
+
+        Arrays.sort(job);
+
+
+        dp[0] = job[0].p;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if (inRange(job[i], job[j])){
+                    dp[i] = Math.max(dp[i], job[j].p + dp[i]);
+                }
+            }
+
+            dp[i] = Math.max(dp[i], job[i].p);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int n : dp) max = Math.max(max, n);
+        System.out.println(max);
+    }
+
+    public static boolean inRange(Job now, Job previous){
+        if (now.s <= previous.e) return false;
+        return true;
+    }
+}
