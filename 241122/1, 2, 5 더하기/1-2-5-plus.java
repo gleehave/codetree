2e65f7e9d5cfa2062abb3d290458166943f32738
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-22 16:22:32 +0900

    Title: 1, 2, 5 더하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241122/1, 2, 5 더하기/1-2-5-plus.java
+++ 241122/1, 2, 5 더하기/1-2-5-plus.java
@@ -1,29 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int[] num = new int[]{0, 1, 2, 5};
-    static int[] dp;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final int MOD = 10007;
+    public static final int MAX_M = 3;
+    public static final int MAX_N = 1000;
     
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n+1];
+    // 변수 선언
+    public static int n;
+    
+    // dp[i] : 합 i를 만들기 위한 서로 다른 가짓수
+    public static int[] dp = new int[MAX_N + 1];
+    public static int[] numbers = new int[]{1, 2, 5};
 
-        for(int i=1; i<=n; i++){
-            dp[i] = Integer.MIN_VALUE;
-        }
-        System.out.println(Arrays.toString(dp));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        
+        // 초기 조건은
+        // 아직 아무 수도 고르지 않았을 때이므로
+        // 합 0을 만들기 위한 가짓수가 1이 되어
+        // dp[0] = 1입니다.
+        dp[0] = 1;
 
-        // DP 배열 업데이트
-        for (int number : num) {
-            for (int i = number; i <= n; i++) {
-                dp[i] += dp[i - number];
+        // 점화식에 따라 값을 채워줍니다.
+        for(int i = 1; i <= n; i++) {
+            // 마지막으로 고른 숫자가 numbers[j]인 경우에 대해
+            // 가짓수를 더해줍니다.
+            for(int j = 0; j < MAX_M; j++) {
+                if(i >= numbers[j])
+                    dp[i] = (dp[i] + dp[i - numbers[j]]) % MOD;
             }
         }
 
-        System.out.println(Arrays.toString(dp));
+        // 합이 n이 되기 위한 가짓수를 출력합니다.
+        System.out.print(dp[n]);
     }
 }
