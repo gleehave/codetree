d88b0f6fa63b17cc48c2edc0f29a046168d37b76
이강호 <91510831+gleehave@users.noreply.github.com>
2024-11-27 22:08:56 +0900

    Title: 계단 오르기 2, Time: 140ms, Memory: 11MB, Status: Passed - CodeTree

--- 241127/계단 오르기 2/climbing-stairs-2.java
+++ 241127/계단 오르기 2/climbing-stairs-2.java
@@ -1,41 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int[] coin;
-    static int[][] dp; // n층 / coin 획득 값
+    // 상수 MAXN을 정의합니다.
+    public static final int MAXN = 1005;
+    // 변수 n과 배열 coin, dp를 정의합니다.
+    public static int n;
+    public static int[] coin = new int[MAXN];
+    public static int[][] dp = new int[MAXN][5];
 
-    public static void main(String[] args) throws Exception {
-        StringTokenizer st;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        n = Integer.parseInt(br.readLine());
-        
-        int maxValue = 0;
-        coin = new int[n+1];
-        st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=n; i++){
-            coin[i] = Integer.parseInt(st.nextToken());
-            maxValue += coin[i];
-        }
+        // 계단의 층 수를 입력받습니다.
+        n = sc.nextInt();
+        // 각 층의 동전의 개수를 입력받습니다.
+        for (int i = 1; i <= n; i++)
+            coin[i] = sc.nextInt();
 
-        dp = new int[n + 1][maxValue + 1];
+        // 기본 케이스를 초기화합니다.
         dp[1][1] = coin[1];
 
-        for(int i=1; i<=n; i++){
+        dp[2][0] = coin[2];
+        dp[2][2] = coin[1] + coin[2];
 
-            for(int j=1; j<=i; j++){
-                if (j - 2 < 0) continue;
-                dp[i][j] = Math.max(dp[i-1][j-1] + coin[i], dp[i-2][j-1] + coin[i]);                
+        // 동적 프로그래밍을 사용하여 최대 가치를 계산합니다.
+        // dp[i][j] : i번 위치에 도착했을 때, 정확히 j번 1계단 올랐을 때의 최대 가치
+        for (int i = 3; i <= n; i++) {
+            for (int j = 0; j <= 3; j++) {
+                if (dp[i-2][j] != 0)
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 2][j] + coin[i]);
+                if (j > 0 && dp[i-1][j-1] != 0)
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + coin[i]);
             }
         }
 
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=maxValue; j++){
-                System.out.print(dp[i][j]+" ");
-            }
-            System.out.println();
-        }
+        // 가능한 모든 경우에서 최대 가치를 찾아 출력합니다.
+        int ans = 0;
+        for (int j = 0; j <= 3; j++)
+            ans = Math.max(ans, dp[n][j]);
+
+        System.out.println(ans);
     }
 }
