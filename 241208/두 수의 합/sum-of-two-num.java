33167a6559c4ec76100e4b8c2ec9c10be8eebbe5
이강호 <91510831+gleehave@users.noreply.github.com>
2024-12-08 16:01:32 +0900

    Title: 두 수의 합, Time: 734ms, Memory: 36MB, Status: Passed - CodeTree

--- 241208/두 수의 합/sum-of-two-num.java
+++ 241208/두 수의 합/sum-of-two-num.java
@@ -1,43 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
-    static int n, k;
-    static List<Integer> removeKey;
-    static Map<Integer, Integer> map = new HashMap<>();
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, k;
+    public static long[] arr = new long[MAX_N];
+    public static HashMap<Long, Integer> count = new HashMap<>();
 
-    public static void main(String[] args) throws Exception {
-        StringTokenizer st;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int ans = 0;
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            int key = Integer.parseInt(st.nextToken());
-            map.put(key, map.getOrDefault(key, 0) + 1);
+        // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+        for(int i = 0; i < n; i++) {
+            long diff = k - arr[i];
+            // 가능한 모든 쌍의 수를 세어줍니다.
+            if(count.containsKey(diff))
+                ans += count.get(diff);
+
+            // 현재 숫자의 개수를 하나 증가시켜줍니다.
+            if(!count.containsKey(arr[i]))
+                count.put(arr[i], 1);
+            else
+                count.put(arr[i], count.get(arr[i]) + 1);
         }
 
-        int count = 0;
-        while(n-- > 0){
-            removeKey = new ArrayList<>();
-
-            for(int key : map.keySet()){
-                if (map.get(key) <= 0) continue;
-
-                if (map.containsKey(k - key)){
-                    count++;
-                    int num = map.get(key);
-                    map.put(key, --num);
-
-                    num = map.get(k - key);
-                    map.put(k-key, --num);
-                }
-            }
-        }
-
-        System.out.println(count);
+        System.out.print(ans);
     }
 }
