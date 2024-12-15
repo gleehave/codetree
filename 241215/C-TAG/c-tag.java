e7184dec9916f8fe0e0c85cca6429bde256d0068
이강호 <91510831+gleehave@users.noreply.github.com>
2024-12-15 15:00:00 +0900

    Title: C-TAG, Time: 3194ms, Memory: 179MB, Status: Passed - CodeTree

--- 241215/C-TAG/c-tag.java
+++ 241215/C-TAG/c-tag.java
@@ -1,62 +1,68 @@
+/*
+M이 최대 50개면, 50개 중에 3개를 뽑는다. 
+선택된 3개의 열을 대상으로 집합 A, 집합 B에 대해서 넣어보자.
+만약 서로 같은게 없으면 조합+1, 그렇지 않으면 Fail
+*/
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static int n, m;
     static String[][] map;
-
+    static HashSet<String> aSet = new HashSet<>();
+    static HashSet<String> bSet = new HashSet<>();
+    
     public static void main(String[] args) throws Exception {
+        StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken()); // 그룹 당 종이 수
-        m = Integer.parseInt(st.nextToken()); // 알파벳 개수 (열 수)
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        map = new String[2 * n][m]; // 2N개의 종이
-        for (int i = 0; i < 2 * n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = String.valueOf(line.charAt(j));
+        map = new String[2 * n][m];
+        for(int i=0; i<2*n; i++){
+            String[] data = br.readLine().split("");
+            for(int j=0; j<m; j++){
+                map[i][j] = data[j];
             }
         }
 
-        int validCombinationCount = 0;
+        int count = 0;
+        for(int i=0; i<m; i++){
+            for(int j=i+1; j<m; j++){
+                for(int k=j+1; k<m; k++){
+                    
+                    afind(i, j, k);
+                    bfind(i, j, k);
 
-        // 열 조합을 3개 선택
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < m; j++) {
-                for (int k = j + 1; k < m; k++) {
-                    // 현재 선택한 열 조합에서 A와 B 그룹의 패턴을 추출
-                    Set<String> aSet = new HashSet<>();
-                    Set<String> bSet = new HashSet<>();
-
-                    // 그룹 A의 패턴 추가
-                    for (int r = 0; r < n; r++) {
-                        aSet.add(map[r][i] + map[r][j] + map[r][k]);
-                    }
-
-                    // 그룹 B의 패턴 추가
-                    for (int r = n; r < 2 * n; r++) {
-                        bSet.add(map[r][i] + map[r][j] + map[r][k]);
-                    }
-
-                    // A와 B의 패턴에 공통이 없다면 유효한 조합
-                    boolean isValid = true;
-                    for (String pattern : aSet) {
-                        if (bSet.contains(pattern)) {
-                            isValid = false;
+                    boolean available = true;
+                    for(String aWord : aSet){
+                        if (bSet.contains(aWord)){
+                            available = false;
                             break;
                         }
                     }
-
-                    if (isValid) {
-                        validCombinationCount++;
-                    }
+                    if (available) count++;
                 }
             }
         }
+        System.out.println(count);
 
-        // 결과 출력
-        System.out.println(validCombinationCount);
+    } // main
+
+    public static void afind(int i, int j, int k){
+        aSet = new HashSet<>();
+        for(int r=0; r<n; r++){
+            aSet.add(map[r][i]+map[r][j]+map[r][k]);
+        }
+    }
+
+    public static void bfind(int i, int j, int k){
+        bSet = new HashSet<>();
+        for(int r=n; r<2 * n; r++){
+            bSet.add(map[r][i]+map[r][j]+map[r][k]);
+        }
     }
 }
