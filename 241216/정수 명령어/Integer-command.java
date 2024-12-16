0150f0073b4e92b5daddfd228d0dbbc1c0223241
이강호 <91510831+gleehave@users.noreply.github.com>
2024-12-16 21:02:06 +0900

    Title: 정수 명령어, Time: 92ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241216/정수 명령어/Integer-command.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.util.TreeSet;
+
+public class Main {
+    static int T, TIME;
+    static TreeSet<Integer> treeSet = new TreeSet<>();
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        T = Integer.parseInt(br.readLine());
+
+        for(int t=0; t<T; t++){
+            TIME = Integer.parseInt(br.readLine());
+            for(int i=0; i<TIME; i++){
+
+                st = new StringTokenizer(br.readLine());
+                String command = st.nextToken();
+                int value = Integer.parseInt(st.nextToken());
+
+                if (command.equals("I")){
+                    treeSet.add(value);
+                } else{
+                    if (!treeSet.isEmpty()){
+                        if (value == 1){
+                            treeSet.remove(treeSet.last());
+                        } else {
+                            treeSet.remove(treeSet.first());
+                        }
+                    }
+                }
+            }
+            
+            if (!treeSet.isEmpty()){
+                System.out.println(treeSet.last() +" "+treeSet.first());
+            } else {
+                System.out.println("EMPTY");
+            }
+        }
+    }
+}
