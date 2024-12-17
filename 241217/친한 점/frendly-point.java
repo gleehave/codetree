e2e238f990b7329e756cf66357d1f918be24b357
이강호 <91510831+gleehave@users.noreply.github.com>
2024-12-17 11:47:21 +0900

    Title: 친한 점, Time: 479ms, Memory: 23MB, Status: Passed - CodeTree

--- 241217/친한 점/frendly-point.java
+++ 241217/친한 점/frendly-point.java
@@ -2,22 +2,22 @@ import java.io.*;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
-class Dot implements Comparable<Dot>{
+class Dot implements Comparable<Dot> {
     int r, c;
-    public Dot(int r, int c){
+
+    public Dot(int r, int c) {
         this.r = r;
         this.c = c;
     }
 
     @Override
-    public int compareTo(Dot a){
-        if (this.r == a.r){
-            return this.c - a.c;
+    public int compareTo(Dot a) {
+        if (this.r == a.r) {
+            return this.c - a.c; // y좌표 기준 정렬
         }
-        return this.r - a.r;
+        return this.r - a.r; // x좌표 기준 정렬
     }
 }
-
 
 public class Main {
     static int n, m;
@@ -27,23 +27,38 @@ public class Main {
         StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 입력
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         treeSet = new TreeSet<>();
-        for(int i=0; i<n; i++){
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             treeSet.add(new Dot(r, c));
         }
 
-        for(int i=0; i<m; i++){
+        // 질의 처리
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            
+            Dot query = new Dot(r, c);
+            Dot result = null;
+            Dot ceiling = treeSet.ceiling(query);
+
+            if (ceiling != null && (ceiling.r > query.r || (ceiling.r == query.r && ceiling.c >= query.c))) {
+                result = ceiling;
+            }
+
+            if (result == null) {
+                System.out.println("-1 -1");
+            } else {
+                System.out.println(result.r + " " + result.c);
+            }
         }
     }
 }
