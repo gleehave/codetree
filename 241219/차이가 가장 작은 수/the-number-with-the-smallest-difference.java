9eee402532cb5a5c9eed62b675e57ed319d414a1
이강호 <91510831+gleehave@users.noreply.github.com>
2024-12-19 11:59:23 +0900

    Title: 차이가 가장 작은 수, Time: 527ms, Memory: 25MB, Status: Passed - CodeTree

--- 241219/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 241219/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -1,37 +1,32 @@
 import java.io.*;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
-    static int n; 
+    static int n;
     static long m;
     static TreeSet<Long> treeSet = new TreeSet<>();
 
     public static void main(String[] args) throws Exception {
-        StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Long.parseLong(st.nextToken());
 
-        for(int i=0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            treeSet.add(Long.parseLong(st.nextToken()));
+        for (int i = 0; i < n; i++) {
+            long num = Long.parseLong(br.readLine());
+            treeSet.add(num);
         }
 
         long result = Long.MAX_VALUE;
-        for(long value : treeSet){
-            if (treeSet.floor(Math.abs(m - value)) == null){
-                continue;
-            }
 
-            if (m <= Math.abs(value - treeSet.floor(Math.abs(m - value)))){
-                result = Math.min(result, Math.abs(value - treeSet.floor(Math.abs(m - value))));
+        for (long value : treeSet) {
+            Long higher = treeSet.higher(value + m - 1);
+            if (higher != null) {
+                result = Math.min(result, higher - value);
             }
         }
 
-        if (result == Long.MAX_VALUE) System.out.println(-1);
-        else System.out.println(result);
-    } // main
+        System.out.println(result == Long.MAX_VALUE ? -1 : result);
+    }
 }
