efe148b782038eaf53fe0a3b005e54fa426e4847
이강호 <91510831+gleehave@users.noreply.github.com>
2024-12-22 13:20:08 +0900

    Title: 최대로 연속한 숫자, Time: 2785ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241222/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 241222/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -1,54 +1,45 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class Main {
     static int n, m;
-    static int[] dp, arr;
-
     static TreeSet<Integer> treeSet = new TreeSet<>();
 
     public static void main(String[] args) throws Exception {
-        StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<=n; i++){
+        for (int i = 0; i <= n; i++) {
             treeSet.add(i);
         }
 
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             treeSet.remove(Integer.parseInt(st.nextToken()));
             System.out.println(countLength());
         }
     }
 
-    public static int countLength(){
-        dp = new int[treeSet.size()];
-        arr = new int[treeSet.size()];
+    public static int countLength() {
+        int maxLength = 0;
+        int currentLength = 0;
+        Integer prev = null;
 
-        int index = 0;
-        for(int n : treeSet) arr[index++] = n;
-
-        Arrays.fill(dp, 1);
-        for(int i=0; i<treeSet.size(); i++){
-            for(int j=0; j<i; j++){
-                if (arr[j] + 1 == arr[j+1]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                } else {
-                    dp[i] = 1;
-                }
+        for (int num : treeSet) {
+            if (prev == null || num == prev + 1) {
+                currentLength++;
+            } else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 1;
             }
+            prev = num;
         }
-
-        int max = -1;
-        for(int n : dp) max = Math.max(max, n);
-
-        return max;
+        maxLength = Math.max(maxLength, currentLength); // 마지막 길이 갱신
+        return maxLength;
     }
 }
